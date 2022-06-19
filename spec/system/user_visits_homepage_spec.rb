f9537181bb340c01c02e67ab42b146ff675ccca6
Sergio Gurgel <sergiogurgell@gmail.com>
2022-06-19 20:59:28 -0300

    Aula 48 - Consumindo APIs: detalhes de um galpão

--- spec/system/user_visits_homepage_spec.rb
+++ spec/system/user_visits_homepage_spec.rb
@@ -23,4 +23,38 @@ describe 'Usuário visita página inicial' do
 
     expect(page).to have_content('Nenhum galpão encontrado')
   end
+
+  it 'e vê detalhes de um galpão' do
+    warehouses_json = File.read(Rails.root.join('spec/support/json/warehouses.json'))
+    fake_response = double('faraday response', status: 200, body: warehouses_json)
+    allow(Faraday).to receive(:get).with('http://localhost:4000/api/v1/warehouses').and_return(fake_response)
+
+    warehouse_json = File.read(Rails.root.join('spec/support/json/warehouse.json'))
+    fake_response = double('faraday_response', status: 200, body: warehouse_json)
+    allow(Faraday).to receive(:get).with('http://localhost:4000/api/v1/warehouses/3').and_return(fake_response)
+
+    visit root_path
+    click_on 'Galpão Fortaleza'
+
+    expect(page).to have_content('Detalhes de FOR - Galpão Fortaleza')
+    expect(page).to have_content('Fortaleza - CE')
+    expect(page).to have_content('34500 m²')
+    expect(page).to have_content('Av. Silas Munguba, 150 - CEP 60818-159')
+    expect(page).to have_content('Galpão de FortalCity')
+  end
+
+  it 'mas não é possível carregar o galpão' do
+    warehouses_json = File.read(Rails.root.join('spec/support/json/warehouses.json'))
+    fake_response = double('faraday_response', status: 200, body: warehouses_json)
+    allow(Faraday).to receive(:get).with('http://localhost:4000/api/v1/warehouses').and_return(fake_response)
+
+    error_response = double('faraday_error_response', status: 500, body: "{}")
+    allow(Faraday).to receive(:get).with('http://localhost:4000/api/v1/warehouses/3').and_return(error_response)
+
+    visit root_path
+    click_on 'Galpão Fortaleza'
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content('Não foi possível carregar o galpão')
+  end
 end
